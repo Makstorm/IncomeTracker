fe8e7913088c4fc3e7bf34fdf5105f69748a6603
makstorm <mxm.horobets@gmail.com>
2026-06-12 12:59:25 +0200

    Initial Commit

---
+++ IncomeTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IncomeTracker
+//
+//  Created by Maxym Horobets on 12.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
